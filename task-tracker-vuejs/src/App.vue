0011878a503ceab562c2e28103fa455f2168e2d1
Shane Dorsey <scdorsey22@gmail.com>
2023-02-05 20:19:10 -0800

    button and tasks setup

--- task-tracker-vuejs/src/App.vue
+++ task-tracker-vuejs/src/App.vue
@@ -1,19 +1,53 @@
 <template>
-<div class="container">
-  <Header title="Task Tracker"/>
-</div>
-
+  <div class="container">
+    <Header title="Task Tracker"/>
+    <Tasks @delete-task="deleteTask" :tasks="tasks"/>
+  </div>
 </template>
 
 <script>
 import Header from './components/Header.vue'
+import Tasks from './components/Tasks.vue'
 
 export default {
   name: 'App',
   components: {
-    Header
+    Header,
+    Tasks,
+  },
+  data() {
+    return {
+      tasks: []
+    }
+  },
+  methods: {
+    deleteTask(id) {
+      console.log('task', id)
+    }
+
+  },
+  created() {
+    this.tasks = [
+      {
+        id: 1,
+        text: "Doctor's Appointment",
+        day: "March 1st at 2:30pm",
+        reminder: true,
+      },
+      {
+        id: 2,
+        text: "Meeting at School",
+        day: "March 3rd at 1:30pm",
+        reminder: true,
+      },
+      {
+        id: 3,
+        text: "Grocery Shopping",
+        day: "March 1st at 11:30am",
+        reminder: false,
+      },
+    ]
   }
-  
 }
 </script>
 
--- task-tracker-vuejs/src/components/Header.vue
+++ task-tracker-vuejs/src/components/Header.vue
@@ -1,16 +1,23 @@
 <template>
     <header>
         <h1>{{ title }}</h1>
+        <Button text="Add Task" color="green" />
     </header>
     
 </template>
 
  <script>
+ import Button from './Button.vue'
+
      export default {
+         
          name: 'Header',
          props: {
              title: String,
-         }
+         },
+         components: {
+             Button
+         },
      }
  </script>
 
